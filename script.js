ccddca561998d0f5d1e6d1367969078b3f897543
Ahmed Hussein <wolktheline@yahoo.com>
2022-10-02 20:29:21 -0600

    using flat() and adding a number as an argument

--- script.js
+++ script.js
@@ -495,4 +495,4 @@ const deposit1 = account4.movements.every(depositFun);
 console.log(deposit1);
 
 const arr = [[[1, 2], 3], [[4, 5], 6], 7, 8];
-console.log(arr.flat());
+console.log(arr.flat(2));
